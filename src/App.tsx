e30632809ff902906a1a4701c2e7067c7ecc6533
Aleksejs Bojarcenko <allexz4fun@gmail.com>
2021-12-02 14:11:11 +0200

    task in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable no-param-reassign */
 /* eslint-disable react/no-array-index-key */
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import ProgressBar from '@ramonak/react-progress-bar';
 import Button from './components/Button/Button';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.scss';
@@ -8,13 +10,46 @@ import './App.scss';
 type Task = {
   name: string;
   completed: boolean;
+  tag: string;
+  edit: boolean;
 };
 
-const App = () => {
-  const [inputValue, setInputValue] = useState('');
+const tags = ['today', 'this week', 'this month'];
 
+// localStorage.setItem('tasks', JSON.stringify([]));
+const App = () => {
+  const [tasks, setTasks] = useState<Task[]>(() => {
+    const saved = localStorage.getItem('toDoList') || '';
+    if (saved) {
+      return JSON.parse(saved);
+    }
+
+    return [];
+  });
+
+  const [inputValue, setInputValue] = useState('');
+  const [allTasks, setAllTasks] = useState<Task[]>([]);
   const [showCompleted, setShowCompleted] = useState(false);
-  const [toDoList, setToDoList] = useState<Task[]>([]);
+  const [progressBar, setProgressBar] = useState(0);
+
+  const [selectedImage, setSelectedImage] = useState<File | null>();
+
+  useEffect(() => {
+    localStorage.setItem('toDoList', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const doneTasks = [...tasks].filter((item) => item.completed).length;
+
+    const progresBarCalculator = () => {
+      if (!tasks.length) {
+        return 0;
+      }
+      return Math.trunc((100 / tasks.length) * doneTasks);
+    };
+    setProgressBar(progresBarCalculator);
+  }, [tasks]);
+
   const input = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -23,12 +58,38 @@ const App = () => {
 
   const addNewTask = () => {
     if (inputValue) {
-      setToDoList([...toDoList, { name: inputValue, completed: false }]);
+      setTasks([
+        ...tasks,
+        {
+          name: inputValue,
+          completed: false,
+          tag: tags[0],
+          edit: false,
+        },
+      ]);
       setInputValue('');
     }
   };
 
-  const taskToShow = toDoList.filter((task) => {
+  const editTask = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number,
+  ) => {
+    const cloneToDoList = [...tasks];
+    cloneToDoList[index].name = event.target.value;
+    setTasks(cloneToDoList);
+  };
+
+  const dropDownValue = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+    index: number,
+  ) => {
+    const cloneToDoList = [...tasks];
+    cloneToDoList[index].tag = event.target.value;
+    setTasks(cloneToDoList);
+  };
+
+  const taskToShow = tasks.filter((task) => {
     if (showCompleted) {
       return task.completed;
     }
@@ -36,23 +97,34 @@ const App = () => {
   });
 
   const clearAll = () => {
-    setToDoList([]);
+    setTasks([]);
   };
 
+  const editActive = (index: number) => {
+    const cloneToDoList = [...tasks];
+    if (cloneToDoList[index].edit === true) {
+      cloneToDoList[index].edit = false;
+    } else {
+      cloneToDoList[index].edit = true;
+    }
+
+    setTasks(cloneToDoList);
+  };
   const taskDone = (index: number) => {
-    const cloneToDoList = [...toDoList];
+    const cloneToDoList = [...tasks];
     if (cloneToDoList[index].completed === true) {
       cloneToDoList[index].completed = false;
     } else {
       cloneToDoList[index].completed = true;
     }
 
-    setToDoList(cloneToDoList);
+    setTasks(cloneToDoList);
   };
 
   const deleteTask = (index: number) => {
-    const newArr = toDoList.filter((task, i) => i !== index);
-    setToDoList(newArr);
+    const cloneToDoList = [...tasks];
+    cloneToDoList.filter((task, i) => i !== index);
+    setTasks(cloneToDoList);
   };
 
   const buttons = [
@@ -70,28 +142,77 @@ const App = () => {
     },
   ];
 
+  const showToday = () => {
+    setAllTasks([...tasks]);
+    const cloneToDoList = [...tasks];
+    return setTasks(cloneToDoList.filter((item) => item.tag === tags[0]));
+  };
+  const showThisWeek = () => {
+    setAllTasks([...tasks]);
+    const cloneToDoList = [...tasks];
+    return setTasks(cloneToDoList.filter((item) => item.tag === tags[1]));
+  };
+  const showThisMonth = () => {
+    setAllTasks([...tasks]);
+    const cloneToDoList = [...tasks];
+    return setTasks(cloneToDoList.filter((item) => item.tag === tags[2]));
+  };
+
+  const showAll = () => setTasks([...allTasks]);
   return (
     <div className="container">
-      <div className="wrapper">
-        <input
-          className="input"
-          type="text"
-          ref={input}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+      <div className="todo">
+        <div className="wrapper">
+          <input
+            className="input"
+            type="text"
+            ref={input}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className="main__buttons">
+            {buttons.map(({ buttonName, onClick }) => (
+              <Button key={buttonName} onClick={onClick}>
+                {buttonName}
+              </Button>
+            ))}
+          </div>
+        </div>
+        <div className="bar">
+          <div className="checkbox">
+            <input
+              className="checkbox__input"
+              type="checkbox"
+              id="done"
+              onChange={() => setShowCompleted(!showCompleted)}
+            />
+            <label className="checkbox__label" htmlFor="done">
+              Completed tasks
+            </label>
+          </div>
+          <div className="tags__buttons">
+            <button className="button button--today" onClick={showToday}>
+              today
+            </button>
+            <button className="button button--week" onClick={showThisWeek}>
+              this week
+            </button>
+            <button className="button button--month" onClick={showThisMonth}>
+              this month
+            </button>
+            <button className="button button--all" onClick={showAll}>
+              All
+            </button>
+          </div>
+        </div>
+        <h1 className="progress__title">Progress bar</h1>
+        <ProgressBar
+          completed={progressBar}
+          bgColor="#733e94"
+          baseBgColor="#94944b"
+          labelColor="#c33131"
         />
-        {buttons.map(({ buttonName, onClick }) => (
-          <Button key={buttonName} onClick={onClick}>
-            {buttonName}
-          </Button>
-        ))}
-
       </div>
-      <label htmlFor="done">
-        <input type="checkbox" id="done" onChange={() => setShowCompleted(!showCompleted)} />
-        {showCompleted ? ' Completed tasks' : 'All tasks' }
-      </label>
-
       <div className="list__wrapper">
         {taskToShow.map((task, index) => (
           <div
@@ -99,8 +220,64 @@ const App = () => {
             className={task.completed ? 'task__item done' : 'task__item'}
             style={{ background: '#DEB887' }}
           >
-            {task.name}
+            <input
+              type="text"
+              value={task.name}
+              disabled={!task.edit}
+              onChange={(e) => editTask(e, index)}
+            />
+            <div>
+              <h1>Upload and Display Image usign React Hooks</h1>
+              {selectedImage && (
+                <div>
+                  <img
+                    alt="not fount"
+                    width="250px"
+                    src={URL.createObjectURL(selectedImage)}
+                  />
+                  <br />
+                  <button onClick={() => setSelectedImage(null)}>Remove</button>
+                </div>
+              )}
+              <br />
+
+              <br />
+              <input
+                type="file"
+                name="myImage"
+                onChange={(event) => {
+                  if (event && event.target && event.target.files) {
+                    setSelectedImage(event.target.files[0]);
+                  }
+                }}
+              />
+            </div>
             <div className="button__wrapper">
+              <button
+                onClick={() => {
+                  editActive(index);
+                }}
+              >
+                {task.edit ? 'save' : 'edit'}
+              </button>
+              <>
+                <select
+                  value={task.tag}
+                  name={task.tag}
+                  style={{
+                    background: '#c02975',
+                    color: 'white',
+                    height: '30px',
+                  }}
+                  onChange={(e) => dropDownValue(e, index)}
+                >
+                  {tags.map((tag) => (
+                    <option key={tag} value={tag}>
+                      {tag}
+                    </option>
+                  ))}
+                </select>
+              </>
               <button
                 className="button button--completed"
                 onClick={() => taskDone(index)}
@@ -115,7 +292,6 @@ const App = () => {
                 X
               </button>
             </div>
-
           </div>
         ))}
       </div>
